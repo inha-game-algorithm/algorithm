4f7acf5571145587ad3a6cde2bf87f50c24528f4
aurenixs <devaiden923@gmail.com>
2026-06-10 14:43:30 +0900

    #9 add: solved K번째수

---
+++ 프로그래머스/K번째수/K번째수_김민성.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+
+    for (int i = 0; i < commands.size(); ++i)
+    {
+        int start = commands[i][0] - 1;
+        int end = commands[i][1];
+        int k = commands[i][2] - 1;
+
+        vector<int> temp(array.begin() + start, array.begin() + end);
+        sort(temp.begin(), temp.end());
+        answer.push_back(temp[k]);
+    }
+
+    return answer;
+}
