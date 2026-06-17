c1241471ac52c9c209dc26f2637bbf694760cc32
inha-go <mygokorea@gmail.com>
2026-06-17 09:23:06 +0900

    Create SortArrayByParity_MYEONGJIN.cpp

---
+++ LeetCode/SortArrayByParity/SortArrayByParity_MYEONGJIN.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums)
+    {
+        vector<int> a(nums);
+        vector<int> r;
+
+        vector<int>::iterator it;
+
+        do
+        {
+            it = find_if(a.begin(), a.end(), [](int i) { return (i % 2) == 0; });
+        
+            if (it != a.end())
+            {
+                r.push_back(*it);
+                it = a.erase(it);
+            }
+        } while (it != a.end());
+
+        r.insert(r.end(), a.begin(), a.end());
+
+        return r;
+    }
+};
+
