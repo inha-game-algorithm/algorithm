0ee9aa531d8204073c0ce1b96f834f66e1609d62
inha-go <mygokorea@gmail.com>
2026-06-19 00:10:37 +0900

    #4 add: solved 나누어떨어지는숫자배열

---
+++ 프로그래머스/나누어떨어지는숫자배열/나누어떨어지는숫자배열_MYEONGJIN.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor)
+{
+    vector<int> answer;
+    for (auto it : arr)
+    {
+        if (it / divisor != 0 && it % divisor == 0)
+            answer.push_back(it);
+    }
+    
+    if (answer.empty())
+        answer.push_back(-1);
+    sort(answer.begin(), answer.end());
+    return answer;
+}
